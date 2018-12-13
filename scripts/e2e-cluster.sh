95eb7032c6704b70a0b93f833e044225de7d5a05
Jérémie Drouet <jeremie.drouet@gmail.com>
2018-12-13 16:18:52 +0100

    update e2e-cluster script
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- scripts/e2e-cluster.sh
+++ scripts/e2e-cluster.sh
@@ -2,66 +2,41 @@
 
 set -xe
 
-USER="${CIRCLE_PROJECT_USERNAME-${USER-local}}"
-VERSION_ID="${CIRCLE_TAG:-${CIRCLE_SHA1}}"
-VERSION_ID="$(echo ${VERSION_ID} | cut -c1-10)"
-TEST_CLUSTER_NAME="testkit-compose-on-kube-${KUBE_ENV_NAME:-${KUBE_VERSION//./}}-${VERSION_ID//./-}-${USER}"
-CLUSTER_NODES=2
-KUBE_CONFIG="$HOME/.kube/${KUBE_ENV_NAME:-${KUBE_VERSION}}/config"
-TESTKIT_CMD=$(which testkit || echo $HOME/testkit)
-
-auth() {
-  test -e "$HOME/.testkit" || {
-    mkdir -p "$HOME/.testkit"
-    echo "$testkit_pem" | base64 -d > "$HOME/.testkit/testkit.pem"
-  }
-  test -e "$HOME/.aws" || {
-    mkdir -p "$HOME/.aws"
-    cat > "$HOME/.aws/credentials" <<EOF
-[default]
-aws_secret_access_key = $aws_secret_access_key
-aws_access_key_id = $aws_access_key_id
-EOF
-  }
-  testkit --driver aws ls
-}
-
-testkit() {
-    $TESTKIT_CMD "$@"
-}
+KUBE_FOLDER="$HOME/.kube/kind-config-$CLUSTER_NAME"
+KUBE_CONFIG="$KUBE_FOLDER/config"
 
 create_cluster() {
-    mkdir -p "$(dirname $KUBE_CONFIG)"
-    testkit create ubuntu_16.04="$CLUSTER_NODES" --kube --kube-version=${KUBE_VERSION} --kube-config "$KUBE_CONFIG.testkit" --driver aws --name "$TEST_CLUSTER_NAME" --debug
-    kubectl --kubeconfig "$KUBE_CONFIG.testkit" config use-context "$TEST_CLUSTER_NAME"
+  docker pull jdrouet/kindest-node:$KUBE_VERSION
+  docker run --rm \
+    --name turkey-filling-$KUBE_VERSION \
+    --network=host \
+    -e KUBE_VERSION=$KUBE_VERSION \
+    -e CLUSTER_NAME=$CLUSTER_NAME \
+    -v $GOPATH/src:/go/src \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v $HOME/.kube:/root/.kube \
+    jdrouet/kind-testing
 }
 
 delete_cluster() {
-  testkit rm --driver aws "$TEST_CLUSTER_NAME" --debug
+  docker rm -f kind-$CLUSTER_NAME-control-plane
 }
 
-docker() {
-  testkit env --driver aws "$TEST_CLUSTER_NAME" -- "$@"
-
+docker-cmd() {
+  KUBECONFIG=$KUBE_FOLDER DOCKER_STACK_ORCHESTRATOR=kubernetes "$@"
 }
 
 case $1 in
-  auth)
-    auth
-    ;;
   create)
-    auth
     create_cluster
     ;;
   delete)
-    auth
     delete_cluster
     ;;
   docker)
-    auth
-    docker "$@"
+    docker-cmd "$@"
     ;;
   *)
-    echo $"Usage: $0 {create|wait|delete}"
+    echo $"Usage: $0 {create|docker|delete}"
     exit 1
 esac
