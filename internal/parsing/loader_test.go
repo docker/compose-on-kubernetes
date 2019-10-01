fc232faf4a7f6ef5fcae2a76608ca3f23c1e0a52
Christopher Crone <christopher.crone@docker.com>
2019-10-01 13:00:37 +0200

    Use upstream yaml.v2
    To mitigate against malicious YAML (as described here:
    https://github.com/kubernetes/kubernetes/issues/83253) we used a patched
    version of yaml.v2. There is now a fix upstream so we can leverage that.
    
    Signed-off-by: Christopher Crone <christopher.crone@docker.com>

--- internal/parsing/loader_test.go
+++ internal/parsing/loader_test.go
@@ -1,18 +1,10 @@
 package parsing
 
 import (
-	"io/ioutil"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
-
-func TestVeryLargeStillLegitComposefile(t *testing.T) {
-	data, err := ioutil.ReadFile("very-large-composefile.yml")
-	assert.NoError(t, err)
-	err = validateYAML(data)
-	assert.NoError(t, err)
-}
 
 func TestYamlBomb(t *testing.T) {
 	data := []byte(`version: "3"
@@ -25,6 +17,6 @@ f: &f [*e,*e,*e,*e,*e,*e,*e,*e,*e]
 g: &g [*f,*f,*f,*f,*f,*f,*f,*f,*f]
 h: &h [*g,*g,*g,*g,*g,*g,*g,*g,*g]
 i: &i [*h,*h,*h,*h,*h,*h,*h,*h,*h]`)
-	err := validateYAML(data)
-	assert.EqualError(t, err, "yaml: exceeded max number of decoded values (100000)")
+	_, err := LoadStackData(data, nil)
+	assert.EqualError(t, err, "yaml: document contains excessive aliasing")
 }
